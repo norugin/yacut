057073c1de3e516af6a14672c264bcc4931d17a1
Evgenii <norugin@gmail.com>
2025-05-19 15:23:55 +0300

    removed utc

--- yacut/models.py
+++ yacut/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 from random import choice
 import string
 
@@ -11,7 +11,7 @@ class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(512), nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.now(UTC))
+    timestamp = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
     def to_dict(self):
         return dict(
